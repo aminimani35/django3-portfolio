e5d36ae8cdaa9203d244d20fc4f5bf1f29dabd1f
Amin Imani <aminpk99@gmail.com>
2020-04-08 22:35:40 +0430

    some changes

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField('عنوان',max_length=100)
-    description = models.CharField('متن نوشته ',max_length=200)
+    title = models.CharField('عنوان',max_length=200)
+    description = models.TextField ('متن نوشته ',max_length=350)
     image =  models.ImageField('تصویر',upload_to='blog/images')
     url = models.URLField('لینک',blank=True)
+    p_date = models.DateField("تاریخ",auto_now=True)
 
 # Create your models here.
